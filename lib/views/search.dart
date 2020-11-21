e592795621e6e9b733f87089d42e8f7b02a2731d
Sampath <sampathpranay02@gmail.com>
2020-11-21 17:48:17 +0530

    modified the search paage and added the screenshots

--- lib/views/search.dart
+++ lib/views/search.dart
@@ -29,65 +29,67 @@ class _SearchState extends State<Search> {
           title: Text("Github Search", style: GoogleFonts.raleway(fontSize: 25,color: Colors.white,fontWeight: FontWeight.bold),)
         ),
         backgroundColor: Colors.deepPurple,
-        body: Container(
-          padding: EdgeInsets.all(12),
-            child: Column(
-              children: [
+        body: SingleChildScrollView(
+          child: Container(
+            padding: EdgeInsets.all(12),
+              child: Column(
+                children: [
 
-                Container(
-                  height: 300,
-                  width: 300,
-                  child: Image.asset('images/git.png'),
-                ),
+                  Container(
+                    height: 300,
+                    width: 300,
+                    child: Image.asset('images/git.png'),
+                  ),
 
-                SizedBox(height: 35,),
+                  SizedBox(height: 35,),
 
-                Container(
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(5),
-                        border: Border.all(color: Colors.white)
-                      ),
-                      padding: EdgeInsets.symmetric(horizontal: 10),
-                      child: TextFormField(
-                        maxLines: null,
-                        style: GoogleFonts.raleway(fontSize: 16,color: Colors.deepPurple),
-                        onSaved: (val) => name = val,
-                        cursorColor: Colors.deepPurple,
-                        cursorWidth: 1,
-                        controller: searchController,
-                        decoration: InputDecoration(
-                          hintText: "Search for People...",
-                          hintStyle: style,
-                          border: InputBorder.none
+                  Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(5),
+                          border: Border.all(color: Colors.white)
+                        ),
+                        padding: EdgeInsets.symmetric(horizontal: 10),
+                        child: TextFormField(
+                          maxLines: null,
+                          style: GoogleFonts.raleway(fontSize: 16,color: Colors.deepPurple),
+                          onSaved: (val) => name = val,
+                          cursorColor: Colors.deepPurple,
+                          cursorWidth: 1,
+                          controller: searchController,
+                          decoration: InputDecoration(
+                            hintText: "Search for People...",
+                            hintStyle: style,
+                            border: InputBorder.none
+                          ),
                         ),
                       ),
-                    ),
 
-                SizedBox(
-                  height: 15
-                ),
+                  SizedBox(
+                    height: 15
+                  ),
 
-                OutlineButton(
-                    onPressed: (){
-                      searchController.text.isEmpty ? null :
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => SearchResults(name: searchController.text),
-                          ));
-                      setState(() {
-                        searchController.text == "";
-                      });
-                    },
-                  borderSide: BorderSide(color: Colors.white),
-                  highlightedBorderColor: Colors.white,
-                  child: Text("Search", style: GoogleFonts.raleway(color: Colors.white,fontSize: 18),),
-                    ),
+                  OutlineButton(
+                      onPressed: (){
+                        searchController.text.isEmpty ? null :
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => SearchResults(name: searchController.text),
+                            ));
+                        setState(() {
+                          searchController.text == "";
+                        });
+                      },
+                    borderSide: BorderSide(color: Colors.white),
+                    highlightedBorderColor: Colors.white,
+                    child: Text("Search", style: GoogleFonts.raleway(color: Colors.white,fontSize: 18),),
+                      ),
 
-              ],
+                ],
+              ),
             ),
-          ),
+        ),
         ),
       );
   }
